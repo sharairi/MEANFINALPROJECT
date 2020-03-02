728aba1ceb85fdc38d2182b60d683d83e5b58ef7
“sharairi” <smart_sharairi@hotmail.com>
2020-03-02 11:09:34 -0700

    final project

---
+++ server/config.js
@@ -0,0 +1,11 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    'mongodb://localhost/smartshopping'
+};
+
+
+// 'mongodb://ramteja:ramteja@ds241019.mlab.com:41019/ecommerce',
+//   port: 3000,
+//   secret: 'RamtejaRepaka2012312321'
